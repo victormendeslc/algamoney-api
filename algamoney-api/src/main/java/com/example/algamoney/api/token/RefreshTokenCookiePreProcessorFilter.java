d5711efc89b836b4173a1a033d848d685cddcd60
victormendes <victormendeslyma@gmail.com>
2017-10-01 22:57:48 -0300

    Movendo o refresh token do cookie para a requisição

---
+++ algamoney-api/src/main/java/com/example/algamoney/api/token/RefreshTokenCookiePreProcessorFilter.java
@@ -0,0 +1,76 @@
+package com.example.algamoney.api.token;
+
+import org.apache.catalina.util.ParameterMap;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Created by victor on 01/10/2017.
+ */
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class RefreshTokenCookiePreProcessorFilter implements Filter {
+
+    public static final String REFRESH_TOKEN = "refresh_token";
+    public static final String URI_OAUTH_TOKEN = "/oauth/token";
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+
+        if (URI_OAUTH_TOKEN.equalsIgnoreCase(req.getRequestURI())
+                && REFRESH_TOKEN.equals(req.getParameter("grant_type"))
+                && req.getCookies() != null) {
+            for (Cookie cookie : req.getCookies()) {
+                if(cookie.getName().equals(REFRESH_TOKEN)){
+                    String refreshToken = cookie.getValue();
+                    req = new MyServletRequestWrapper(req,refreshToken);
+                }
+            }
+
+        }
+
+        chain.doFilter(req,response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    static class MyServletRequestWrapper extends HttpServletRequestWrapper{
+
+        private final String refreshToken;
+
+        /**
+         * Constructs a request object wrapping the given request.
+         *
+         * @param request The request to wrap
+         * @throws IllegalArgumentException if the request is null
+         */
+        public MyServletRequestWrapper(HttpServletRequest request,String refreshToken) {
+            super(request);
+            this.refreshToken = refreshToken;
+        }
+
+        @Override
+        public Map<String, String[]> getParameterMap() {
+            ParameterMap<String,String[]> map = new ParameterMap<>(getRequest().getParameterMap());
+            map.put(REFRESH_TOKEN,new String[]{refreshToken});
+            return map;
+        }
+    }
+}
